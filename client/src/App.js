7e5d9ca26850d8776ac9a9e0606188fa183ae0b8
PercTruman <drshawnhart@gmail.com>
2022-11-23 11:14:23 -0600

    load index.js and App.js files and create routes

--- client/src/App.js
+++ client/src/App.js
@@ -1,23 +1,42 @@
 import logo from './logo.svg';
 import './App.css';
+import { Routes, Route } from "react-router-dom";
+import { ThemeProvider } from "@mui/material/styles";
+import Welcome from './Pages/Welcome';
+import Signup from './Pages/Signup';
+import Login from './Pages/Login';
+import DogDetail from './Pages/DogDetail';
+import DogRegistration from './Pages/DogRegistration';
+import NewSighting from './Pages/NewSighting';
+import RegisteredDogGallery from './Pages/RegisteredDogGallery';
+import SeenDogs from './Pages/SeenDogs';
+import Sightings from './Pages/Sightings';
+import LostDog from './Pages/LostDog';
+
 
 function App() {
   return (
-    <div className="App">
-      <header className="App-header">
-        <img src={logo} className="App-logo" alt="logo" />
-        <p>
-          Edit <code>src/App.js</code> and save to reload.
-        </p>
-        <a
-          className="App-link"
-          href="https://reactjs.org"
-          target="_blank"
-          rel="noopener noreferrer"
-        >
-          Learn React
-        </a>
-      </header>
+    <div className="App"
+    style={{
+      backgroundImage: `url(${LostDog})`,
+      height: "100vh",
+      width: "auto",
+      backgroundSize: "cover"
+    }} >
+      <ThemeProvider  theme={theme}>
+        <Routes>
+          <Route path="/" element={<Welcome />} />
+          <Route path="/-login" element={<Login />} />
+          <Route path="/-signup" element={<Signup />} />
+          <Route path="/-sightings" element={<Sightings />} />
+          <Route path="/-seen_dogs" element={<SeenDogs />} />
+          <Route path="/-registered_dogs" element={<RegisteredDogGallery />} />
+          <Route path="/-new_sighting" element={<NewSighting />} />
+          <Route path="/-dog_registration" element={<DogRegistration />} />
+          <Route path="/-registered_dogs/:id" element={<DogDetail />} />
+          <Route path="/-seen_dogs/:id" element={<DogDetail />} />
+        </Routes>
+      </ThemeProvider>
     </div>
   );
 }
--- client/src/index.js
+++ client/src/index.js
@@ -1,14 +1,18 @@
-import React from 'react';
-import ReactDOM from 'react-dom';
-import './index.css';
-import App from './App';
+import React from "react";
+import ReactDOM from "react-dom";
+import "./index.css";
+import App from "./App";
+import { UserProvider } from "./context/UserContext";
+import { BrowserRouter as Router } from "react-router-dom";
 
+const root = ReactDOM.createRoot(document.getElementById("root"));
 
-ReactDOM.render(
+root.render(
   <React.StrictMode>
-    <App />
-  </React.StrictMode>,
-  document.getElementById('root')
+    <UserProvider>
+      <Router>
+        <App />
+      </Router>
+    </UserProvider>
+  </React.StrictMode>
 );
-
-

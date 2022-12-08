eede848f9d94a59e1dcb1b51ab8d00421ec3b440
PercTruman <drshawnhart@gmail.com>
2022-12-08 15:57:35 -0600

    add lost dog routes

--- config/routes.rb
+++ config/routes.rb
@@ -1,9 +1,14 @@
 Rails.application.routes.draw do
   get "/me", to: "users#show"
+  get "/lost_dogs", to: "lost_dogs#index"
 
   post "/signup", to: "users#create"
   post "/login", to: "sessions#create"
   post "/lost_dogs", to: "lost_dogs#create"
+
+  patch "/lost_dogs/:id", to: "lost_dogs#update"
+
+  delete "/lost_dogs/:id", to: "lost_dogs#destroy"
   
   get "*path", to: "fallback#index", constraints: ->(req) { !req.xhr? && req.format.html? }
 end

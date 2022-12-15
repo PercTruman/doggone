fb9535e0e2ee1c5d2167e6eae8de650f1926c3ed
PercTruman <drshawnhart@gmail.com>
2022-12-15 16:57:21 -0600

    add MUI grid item list for rendering pictures

--- client/src/Pages/SeenDogs.js
+++ client/src/Pages/SeenDogs.js
@@ -1,29 +1,35 @@
-import React, {useState, useEffect} from 'react'
-import Navbar from '../Components/Navbar';
+import React, { useState, useEffect } from "react";
+import Navbar from "../Components/Navbar";
+import Box from "@mui/material/Box";
+import ImageList from '@mui/material/ImageList';
+import ImageListItem from '@mui/material/ImageListItem';
+
+import Grid from "@mui/material/Unstable_Grid2";
+import Paper from "@mui/material/Paper";
 
 function SeenDogs() {
-  const [imageGallery, setImageGallery] = useState([])
+  const [imageGallery, setImageGallery] = useState(null);
   useEffect(() => {
     fetch("/lost_dogs")
-    .then((res) => res.json()).then((data) => {
-  console.log(data)
-      makeGallery(data)
-      }, [])
-  })
+      .then((res) => res.json())
+      .then((data) => {
+        setImageGallery(data);
+      }, [imageGallery]);
+  });
 
-  function makeGallery(dogObjects) {
-     const imageArray = dogObjects.data.map(dogObject =>
-      (dogObject.attributes.image_url)).map(url =>
-        <img src={url} alt="dog picture"/>)
-// setImageGallery(imageArray)
-  }
+  const imageArray =  imageGallery && imageGallery.data
+    .map((dogObject) => dogObject.attributes.image_url)
+    .map((url, index) => <ImageListItem  key={index}> <img src={url}  loading="lazy" alt="dog picture"/>  </ImageListItem>);
+
 
 return (
-  <div>
+ <Box>
     <Navbar />
-   {imageGallery}
-</div>
-)
+   <ImageList>
+    <h1 style={{marginTop: "5rem"}}> Seen Dogs</h1>
+    {imageArray}
+    </ImageList>
+  </Box>
+);
 }
-
 export default SeenDogs;

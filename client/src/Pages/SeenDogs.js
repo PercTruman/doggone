b98c7a49c35326d4ef45afd0a3305aff6b825799
PercTruman <drshawnhart@gmail.com>
2022-12-16 17:08:41 -0600

    add colored buttons in InfoBar to either addSighting or to get Info

--- client/src/Pages/SeenDogs.js
+++ client/src/Pages/SeenDogs.js
@@ -3,17 +3,20 @@ import { useNavigate, useParams } from "react-router-dom";
 import Navbar from "../Components/Navbar";
 import Box from "@mui/material/Box";
 import ImageList from "@mui/material/ImageList";
+import Button from "@mui/material/Button";
 import ImageListItem from "@mui/material/ImageListItem";
 import ImageListItemBar from "@mui/material/ImageListItemBar";
-import IconButton from '@mui/material/IconButton';
-import InfoIcon from '@mui/icons-material/Info';
+import IconButton from "@mui/material/IconButton";
+import InfoIcon from "@mui/icons-material/Info";
 import Typography from "@mui/material/Typography";
 import Grid from "@mui/material/Unstable_Grid2";
+import VisibilityIcon from "@mui/icons-material/Visibility";
+import { padding } from "@mui/system";
 
 function SeenDogs() {
   const [imageGallery, setImageGallery] = useState(null);
-  const navigate = useNavigate()
-  const {id} = useParams()
+  const navigate = useNavigate();
+  const { id } = useParams();
   useEffect(() => {
     getDogs();
   }, []);
@@ -28,7 +31,6 @@ function SeenDogs() {
   const fullDogObjects =
     imageGallery &&
     imageGallery.data.filter((dog) => (dog.attributes.image_url ? dog : null));
-
 
   return (
     <Box>
@@ -52,32 +54,40 @@ function SeenDogs() {
           sx={{ paddingTop: "5rem", width: "75vw", height: "60vh" }}
           cols={3}
         >
-      {fullDogObjects && fullDogObjects
-          .map((dogObject) =>
-          
-          
-            <ImageListItem key={dogObject.id}>
-              {" "}
-              <img
-                src={dogObject.attributes.image_url}
-                loading="lazy"
-                alt="doggy"
-              />{" "}
-              <ImageListItemBar
-                title= {<span>{dogObject.attributes.breed}</span> }
-                subtitle={<span> {dogObject.attributes.age_group}</span>}
-                actionIcon={
-                  <IconButton
-                    sx={{ color: 'rgba(255, 255, 255, 0.54)' }}
-                    aria-label={`info about ${dogObject.id}`}
-                    onClick={()=>navigate(`/-seen_dogs/${dogObject.id}`)}
-                  >
+          {fullDogObjects &&
+            fullDogObjects.map((dogObject) => (
+              <ImageListItem key={dogObject.id}>
+                {" "}
+                <img
+                  src={dogObject.attributes.image_url}
+                  loading="lazy"
+                  alt="doggy"
+                />{" "}
+                <ImageListItemBar
+                  title={<span>{dogObject.attributes.breed}</span>}
+                  subtitle={<span> {dogObject.attributes.age_group}</span>}
+                  // actionIcon={
+                  //   <IconButton
+                  //     sx={{ color: "rgba(255, 255, 255, 0.54)" }}
+                  //     aria-label={`info about ${dogObject.id}`}
+                  //     onClick={() => navigate(`/-seen_dogs/${dogObject.id}`)}
+                  //   >
+                  //     <InfoIcon />
+                  //   </IconButton>
+                  // }
+                />{" "}
+    
+                <Box>
+                  <IconButton sx={{ color: "#85BBCC" }}>
+                    <VisibilityIcon />
+                  </IconButton>
+
+                  <IconButton sx={{ color: "#F6E89D" }}>
                     <InfoIcon />
                   </IconButton>
-                  }
-              />{" "}
-            </ImageListItem>
-        )}
+                </Box>
+              </ImageListItem>
+            ))}
         </ImageList>
       </Grid>
     </Box>

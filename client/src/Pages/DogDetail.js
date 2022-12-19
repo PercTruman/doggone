f9b0d5cd6a97e3c32a9a49b6bade3f61569544cf
PercTruman <drshawnhart@gmail.com>
2022-12-19 08:33:54 -0600

    complete formatting of dogdetail image and infoBox

--- client/src/Pages/DogDetail.js
+++ client/src/Pages/DogDetail.js
@@ -1,8 +1,9 @@
 import React, { useState, useEffect } from "react";
 import { useParams } from "react-router-dom";
 import Navbar from "../Components/Navbar";
-import { Box } from "@mui/system";
-import Grid2 from '@mui/material/Unstable_Grid2'
+import { Box, color } from "@mui/system";
+import Grid2 from "@mui/material/Unstable_Grid2";
+import { Typography } from "@mui/material";
 
 function DogDetail() {
   const { id } = useParams();
@@ -27,13 +28,45 @@ function DogDetail() {
       );
   }
   return (
-    <Box >
+    <Box>
       <Navbar />
-      <Grid2 container border={'2px solid red'}width={'50%'} height={'300px'} margin={'7rem auto'}>
-        {dogDetails && 
-           <img style={{'width':'100%', 'height':'100%'}}src={dogDetails.picture} alt={"specific dog picture"} />}
-           
-           
+      <Grid2
+        container
+        spacing={0}
+        direction="column"
+        alignItems={"center"}
+        justifyContent={"center"}
+        style={{ minHeight: "10vh" }}
+        paddingTop={"1rem"}
+      >
+        <Typography variant="h2" align="center" style={{ color: "#85BBCC" }}>
+          {" "}
+          Dog Detail
+        </Typography>
+      </Grid2>
+      <Grid2
+        container
+        justifyContent={'center'}
+       
+        width={"500px"}
+        height={"400px"}
+        margin={"3rem auto"}
+      >
+        {dogDetails && (
+          <img
+            style={{ width: "400px", height: "300px" }}
+            src={dogDetails.picture}
+            alt={"specific dog picture"}
+            border={"2px solid #85BBCC"}
+          />
+        )}
+        <Grid2 margin={'1rem auto'} padding={'1rem'} backgroundColor={"darkGray"}>
+        <p style={{fontWeight:'bold'}}>Breed:  {dogDetails.breed}</p>
+          <p style={{fontWeight:'bold'}}>Age Range:  {dogDetails.age.toLowerCase()}</p>
+          <p style={{fontWeight:'bold'}}>Sex:  {dogDetails.sex.toLowerCase()}</p>
+
+         
+        </Grid2>
       </Grid2>
     </Box>
   );

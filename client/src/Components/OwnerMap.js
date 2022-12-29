4cbd04f723af9ac0e23df227f61dd4beb7dba90d
PercTruman <drshawnhart@gmail.com>
2022-12-29 11:41:43 -0600

    format dog sightings page and remove unnecessary buttons

--- client/src/Components/OwnerMap.js
+++ client/src/Components/OwnerMap.js
@@ -1,132 +1,148 @@
-import React, { useState, useMemo, useCallback, useRef } from "react";
-import Navbar from "./Navbar";
+import React, { useState, useMemo, useCallback, useRef } from 'react';
+
 import {
-  useLoadScript,
-  GoogleMap,
-  MarkerF,
-  InfoWindowF,
-} from "@react-google-maps/api";
+	useLoadScript,
+	GoogleMap,
+	MarkerF,
+	InfoWindowF,
+} from '@react-google-maps/api';
 
-import dogPaw from "../Pages/dog-paw.svg";
-import Button from "@mui/material/Button";
-
+import dogPaw from '../Pages/dog-paw.svg';
 
 const mapContainerStyle = {
-  margin: "0 auto",
-  width: "50vw",
-  height: "40vh",
+	margin: '0 auto',
+	width: '60vw',
+	height: '40vh',
 };
 
 const options = {
-  disableDefaultUI: true,
-  zoomControl: true,
+	disableDefaultUI: true,
+	zoomControl: true,
 };
 
-function OwnerMap({ setShowOwnerMap, sightingsArray }) {
-  const [selected, setSelected] = useState(null);
-  const [showContactInfo, setShowContactInfo] = useState(false);
-  const center = useMemo(() => ({ lat: 32.59048, lng: -97.04098 }), []);
-  const mapRef = useRef();
-  const { isLoaded } = useLoadScript({
-    googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
-  });
+function OwnerMap({ sightingsArray }) {
+	const [selected, setSelected] = useState(null);
+	const [showContactInfo, setShowContactInfo] = useState(false);
+	const center = useMemo(() => ({ lat: 32.59048, lng: -97.04098 }), []);
+	const mapRef = useRef();
+	const { isLoaded } = useLoadScript({
+		googleMapsApiKey: process.env.REACT_APP_GOOGLE_MAPS_API_KEY,
+	});
 
-  const onMapLoad = useCallback((map) => {
-    mapRef.current = map;
-  }, []);
+	const onMapLoad = useCallback((map) => {
+		mapRef.current = map;
+	}, []);
 
-  function compareSightingTimes(a, b) {
-    return a - b;
-  }
+	function compareSightingTimes(a, b) {
+		return a - b;
+	}
 
+	console.log(sightingsArray);
 
-  console.log(sightingsArray)
+	const sortedSightingsArray = sightingsArray.sort(compareSightingTimes);
+	const markers =
+		window.google &&
+		sightingsArray.map((sighting) => {
+			const date = new Date(sighting.created_at).toLocaleString();
+			return (
+				<div key={sighting.id}>
+					<MarkerF
+						position={{
+							lat: Number(sighting.lat),
+							lng: Number(sighting.lng),
+						}}
+						icon={{
+							url: dogPaw,
+							scaledSize: new window.google.maps.Size(20, 20),
+							origin: new window.google.maps.Point(0, 0),
+							anchor: new window.google.maps.Point(15, 15),
+						}}
+					/>
+					<InfoWindowF
+						options={{
+							pixelOffset: new window.google.maps.Size(0, -15),
+						}}
+						maxWidth={50}
+						position={{
+							lat: Number(sighting.lat),
+							lng: Number(sighting.lng),
+						}}
+						onCloseClick={() => {
+							setSelected(null);
+						}}
+					>
+						<div style={{ maxHeight: '100px', maxWidth: '75px' }}>
+							<h3 style={{ color: 'red' }}>
+								{sortedSightingsArray.indexOf(sighting) + 1}
+							</h3>
+							<p>Seen at: {date}</p>
+							{sighting.contact_method ? (
+								<div style={{ fontWeight: 'bold' }}>
+									{' '}
+									{sighting.contact_method}:
+									<br />
+									blah
+								</div>
+							) : null}
+						</div>
+					</InfoWindowF>{' '}
+					{showContactInfo ? (
+						<InfoWindowF
+							options={{
+								pixelOffset: new window.google.maps.Size(
+									0,
+									-15
+								),
+							}}
+							maxWidth={50}
+							position={{
+								lat: Number(sighting.lat),
+								lng: Number(sighting.lng),
+							}}
+							onCloseClick={() => {
+								setSelected(null);
+							}}
+						>
+							<div
+								style={{ maxHeight: '100px', maxWidth: '75px' }}
+							>
+								<h3 style={{ color: 'red' }}>
+									{sighting.contact_method}
+								</h3>
+							</div>
+						</InfoWindowF>
+					) : null}
+				</div>
+			);
+		});
 
-  const sortedSightingsArray = sightingsArray.sort(compareSightingTimes);
-  const markers =
-    window.google &&
-    sightingsArray.map((sighting) => {
-      const date = new Date(sighting.created_at).toLocaleString();
-      return (
-        <div key={sighting.id}>
-          <MarkerF
-            position={{
-              lat: Number(sighting.lat),
-              lng: Number(sighting.lng),
-            }}
-            icon={{
-              url: dogPaw,
-              scaledSize: new window.google.maps.Size(20, 20),
-              origin: new window.google.maps.Point(0, 0),
-              anchor: new window.google.maps.Point(15, 15),
-            }}
-          />
-          <InfoWindowF
-            options={{ pixelOffset: new window.google.maps.Size(0, -15) }}
-            maxWidth={50}
-            position={{ lat: Number(sighting.lat), lng: Number(sighting.lng)}}
-            onCloseClick={() => {
-              setSelected(null);
-            }}
-          >
-            <div style={{ maxHeight: "100px", maxWidth: "75px" }}>
-              <h3 style={{ color: "red" }}>
-                {sortedSightingsArray.indexOf(sighting) + 1}
-              </h3>
-              <p>Seen at: {date}</p>
-              {sighting.contact_method ? (
-                <div style={{fontWeight:'bold'}}> {sighting.contact_method}:
-                <br/>
-                blah</div>
-           
-              ) : null}
-            </div>
-          </InfoWindowF>{" "}
-          {showContactInfo ? (
-            <InfoWindowF
-              options={{ pixelOffset: new window.google.maps.Size(0, -15) }}
-              maxWidth={50}
-              position={{ lat: Number(sighting.lat), lng: Number(sighting.lng) }}
-              onCloseClick={() => {
-                setSelected(null);
-              }}
-            >
-              <div style={{ maxHeight: "100px", maxWidth: "75px" }}>
-                <h3 style={{ color: "red" }}>{sighting.contact_method}</h3>
-              </div>
-            </InfoWindowF>
-          ) : null}
-        </div>
-      );
-    });
+	if (!isLoaded) return <div>Loading...</div>;
 
-  if (!isLoaded) return <div>Loading...</div>;
-
-  return (
-    window.google &&
-    <div>
-   
-    <div style={{ paddingTop: "3rem" }}>
-      {/* <h2 style={{ color: "#85BBCC" }}>Sightings for this dog:</h2> */}
-      <GoogleMap
-        mapContainerStyle={mapContainerStyle}
-        zoom={17}
-        center={center}
-        onLoad={onMapLoad}
-        options={options}
-      >
-        {markers ? markers : null}
-      </GoogleMap>
-      {/* <Button
+	return (
+		window.google && (
+			<div>
+				<div style={{ paddingTop: '3rem' }}>
+					{/* <h2 style={{ color: "#85BBCC" }}>Sightings for this dog:</h2> */}
+					<GoogleMap
+						mapContainerStyle={mapContainerStyle}
+						zoom={16}
+						center={center}
+						onLoad={onMapLoad}
+						options={options}
+					>
+						{markers ? markers : null}
+					</GoogleMap>
+					{/* <Button
         variant="contained"
         onClick={() => setShowOwnerMap((showOwnerMap) => !showOwnerMap)}
         sx={{ height: "45px", marginTop: "2.5rem" }}
       >
         Back to Details
       </Button> */}
-    </div>
-    </div>
-  );
+				</div>
+			</div>
+		)
+	);
 }
 
 export default OwnerMap;
--- client/src/Pages/DogDetail.js
+++ client/src/Pages/DogDetail.js
@@ -1,112 +1,107 @@
-import React, { useState, useEffect } from "react";
-import { useParams } from "react-router-dom";
-import Navbar from "../Components/Navbar";
-import { Box } from "@mui/system";
-import Grid2 from "@mui/material/Unstable_Grid2";
-import { Typography } from "@mui/material";
-import OwnerMap from "../Components/OwnerMap";
-import Button from "@mui/material/Button";
+import React, { useState, useEffect } from 'react';
+import { useParams } from 'react-router-dom';
+import Navbar from '../Components/Navbar';
+import { Box } from '@mui/system';
+import Grid2 from '@mui/material/Unstable_Grid2';
+import { Typography } from '@mui/material';
+import OwnerMap from '../Components/OwnerMap';
+import Button from '@mui/material/Button';
 
 function DogDetail() {
-  const { id } = useParams();
-  const [showOwnerMap, setShowOwnerMap] = useState(false);
-  const [dogDetails, setDogDetails] = useState(null);
-  const [sightingsArray, setSightingsArray] = useState([]);
-  useEffect(() => {
-    getDogDetails();
-  }, []);
+	const { id } = useParams();
+	const [showOwnerMap, setShowOwnerMap] = useState(false);
+	const [dogDetails, setDogDetails] = useState(null);
+	const [sightingsArray, setSightingsArray] = useState([]);
+	useEffect(() => {
+		getDogDetails();
+	}, []);
 
-  function getDogDetails() {
-    fetch(`/seen_dogs/${id}`)
-      .then((res) => res.json())
-      .then((data) => {
-        buildMarkerObjects(data);
+	function getDogDetails() {
+		fetch(`/seen_dogs/${id}`)
+			.then((res) => res.json())
+			.then((data) => {
+				buildMarkerObjects(data);
 
-        function buildMarkerObjects(data) {
-          const arr = [];
-          for (let i = 0; i < data.sightings.length; i++) {
-            arr[i] = {
-              id: data.sightings[i].id,
-              lat: data.sightings[i].latitude,
-              lng: data.sightings[i].longitude,
-              contact_method: data.sightings[i].contact_method,
-              created_at: data.sightings[i].created_at,
-            };
-            setSightingsArray(arr);
-          }
-        }
+				function buildMarkerObjects(data) {
+					const arr = [];
+					for (let i = 0; i < data.sightings.length; i++) {
+						arr[i] = {
+							id: data.sightings[i].id,
+							lat: data.sightings[i].latitude,
+							lng: data.sightings[i].longitude,
+							contact_method: data.sightings[i].contact_method,
+							created_at: data.sightings[i].created_at,
+						};
+						setSightingsArray(arr);
+					}
+				}
 
-        setDogDetails({
-          picture: data.image_url,
-          age: data.age_group,
-          sex: data.sex,
-          breed: data.breed,
-          color: data.color,
-        });
-      });
-  }
+				setDogDetails({
+					picture: data.image_url,
+					age: data.age_group,
+					sex: data.sex,
+					breed: data.breed,
+					color: data.color,
+				});
+			});
+	}
 
-  if (!dogDetails) return null;
+	if (!dogDetails) return null;
 
-  return (
-    <Box>
-      <Navbar />
-      <Grid2
-        container
-        spacing={0}
-        direction="column"
-        alignItems={"center"}
-        justifyContent={"center"}
-        style={{ minHeight: "10vh" }}
-        paddingTop={"1rem"}
-      >
-        <Typography variant="h4" align="center" style={{ color: "#85BBCC" }}>
-          {" "}
-          Dog Sightings
-        </Typography>
-      </Grid2>
-      <Grid2
-        container
-        justifyContent={"center"}
-        width={"300px"}
-        height={"200px"}
-        margin={"1rem auto"}
-      >
-        {dogDetails && (
-          <Box sx={{ border: "2px solid red" }}>
-            <img
-              style={{ width: "300px", height: "200px" }}
-              src={dogDetails.picture}
-              alt={"specific dog picture"}
-              border={"2px solid #85BBCC"}
-            />
-            <div
-              style={{
-                padding: "1rem",
-                backgroundColor: "darkGray",
-                border: "2px solid black",
-              }}
-            >
-              <p style={{ fontWeight: "bold" }}>Breed: {dogDetails.breed}</p>
-              <p style={{ fontWeight: "bold" }}>
-                Age Range: {dogDetails.age.toLowerCase()}
-              </p>
-              <p style={{ fontWeight: "bold" }}>
-                Sex: {dogDetails.sex.toLowerCase()}
-              </p>
-            </div>
-          </Box>
-        )}
-      </Grid2>
-
-      <OwnerMap
-        setShowOwnerMap={setShowOwnerMap}
-        showOwnerMap={showOwnerMap}
-        sightingsArray={sightingsArray}
-        dogDetails={dogDetails}
-      />
-    </Box>
-  );
+	return (
+		<Box>
+			<Navbar />
+			<Grid2
+				container
+				spacing={0}
+				direction='column'
+				alignItems={'center'}
+				justifyContent={'center'}
+				style={{ minHeight: '10vh' }}
+				paddingTop={'1rem'}
+			>
+				<Typography
+					variant='h4'
+					align='center'
+					style={{ color: '#85BBCC' }}
+				>
+					{' '}
+					Dog Sightings
+				</Typography>
+			</Grid2>
+			<Grid2 container>
+				{dogDetails && (
+					<Box
+						sx={{
+							display: 'flex',
+							margin: '0 auto',
+						}}
+					>
+						<img
+							style={{ width: '300px', height: '200px' }}
+							src={dogDetails.picture}
+							alt={'specific dog picture'}
+						/>
+						<Typography style={{ fontWeight: 'bold' }}>
+							Breed: {dogDetails.breed}
+						</Typography>
+						<Typography style={{ fontWeight: 'bold' }}>
+							Age Range: {dogDetails.age.toLowerCase()}
+						</Typography>
+						<Typography style={{ fontWeight: 'bold' }}>
+							Sex: {dogDetails.sex.toLowerCase()}
+						</Typography>
+						<OwnerMap
+							setShowOwnerMap={setShowOwnerMap}
+							showOwnerMap={showOwnerMap}
+							sightingsArray={sightingsArray}
+							dogDetails={dogDetails}
+						/>
+					</Box>
+				)}
+			</Grid2>
+		</Box>
+	);
 }
 
 export default DogDetail;

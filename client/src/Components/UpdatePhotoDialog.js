addd11606da52219bcf8e29c8d3b829f2ab69e0c
PercTruman <drshawnhart@gmail.com>
2023-01-04 20:55:50 -0600

    remove excess code from UpdatePhotoDialog

--- client/src/Components/UpdatePhotoDialog.js
+++ client/src/Components/UpdatePhotoDialog.js
@@ -1,13 +1,7 @@
 import React, { useEffect, useState } from 'react';
 import { useNavigate } from 'react-router-dom';
 import Button from '@mui/material/Button';
-import Dialog from '@mui/material/Dialog';
-import DialogActions from '@mui/material/DialogActions';
-import DialogContent from '@mui/material/DialogContent';
-import DialogTitle from '@mui/material/DialogTitle';
-import IconButton from '@mui/material/IconButton';
-import { Input } from '@mui/material';
-import { Box } from '@mui/system';
+
 
 export default function UpdatePhotoDialog({ id, dogDetails, setDogDetails }) {
 	const [open, setOpen] = useState(false);
@@ -24,14 +18,6 @@ export default function UpdatePhotoDialog({ id, dogDetails, setDogDetails }) {
         }
     }, [selectedImage])
 
-	const handleClose = () => {
-		setOpen(false);
-	};
-
-	const handleSubmit = (e) => {
-		e.preventDefault();
-		buildDogImage(e);
-	};
 
 	function buildDogImage(e) {
         setSelectedImage(e.target.files[0])
@@ -48,12 +34,13 @@ export default function UpdatePhotoDialog({ id, dogDetails, setDogDetails }) {
 			.then((res) =>
 				res.json().then((data) => {
 					setDogDetails({ ...dogDetails, picture: data.image_url });
+                    alert('Dog image was successfully updated!')
 					navigate('/-dogs');
 				})
 			)
 			.catch((err) => console.error(err));
 	}
-console.log(selectedImage)
+
 	return (
 		<div>
             <input accept='image' type='file' id='select-image'style={{display: 'none'}} onChange={e=>buildDogImage(e)}/>
@@ -67,36 +54,6 @@ console.log(selectedImage)
 				Update Photo
 			</Button>
             </label>
-            {imageUrl && selectedImage && (
-        <Box mt={2} textAlign="center">
-          <div>Image Preview:</div>
-          <img src={imageUrl} alt={selectedImage.name} height="100px" />
-        </Box>
-      )}
-			{/* <Dialog open={open} onClose={handleClose}>
-				<form onSubmit={(e) => handleSubmit(e, id)}>
-					<DialogTitle>Update Photo</DialogTitle>
-					<DialogContent sx={{ background: 'white' }}>
-						<label htmlFor='icon-button-file'>
-							<Input accept='image/*' id='icon-button-file' type='file' />
-							<IconButton
-								color='primary'
-								aria-label='upload picture'
-								component='span'
-								onClick={handleClickOpen}
-							>
-								Update Photo
-							</IconButton>
-						</label>
-					</DialogContent>
-					<DialogActions>
-						<Button onClick={handleClose}>Cancel</Button>
-						<Button type='submit' id={id}>
-							Upload Photo
-						</Button>
-					</DialogActions>
-				</form>
-			</Dialog> */}
 		</div>
 	);
 }
